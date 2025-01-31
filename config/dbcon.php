b1dc1cdbb52148bf921c5955e29421963e6e085d
Ives-Lopez <iveslopez64@gmail.com>
2025-01-31 09:40:08 +0800

    #modifide:"label part"

---
+++ config/dbcon.php
@@ -0,0 +1,11 @@
+<?php
+$servername="127.0.0.1";
+$username="root";
+$password="";
+$dbname="db_users";
+
+//connection
+$conn=new mysql($servername, $username, $password, $dbname);
+
+
+?>
--- index.php
+++ index.php
@@ -6,6 +6,18 @@
     <title>Document</title>
 </head>
 <body>
+
+<h1>Display Text Input Fields</h1>
+
+<form action="/action_page.php">
+  <label for="fname">First name: </label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Last name: </label>
+  <input type="text" id="lname" name="lname"><br><br>
+  
+  <input type="submit" value="Add">
+</form>
     
+
 </body>
 </html>
