c34a101d8bb7b5a60d8a9a43358b2ce284477a32
Ives-Lopez <iveslopez64@gmail.com>
2025-02-01 21:46:36 +0800

    #modifide:"style, table, conn"

--- index.php
+++ index.php
@@ -1,37 +1,49 @@
 <?php
 include'dbconn.php';
+include'bootstrap.php';
 ?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
   </head>
-  <body>
-    <nav class></navclass>
+<body>
+<h1 id="main">CRUD USER LIST</h1>
+<h2 id="cyan">User List</h2>
 
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<div class="container mt-3">
+  <table class="table table-hover">
+    <thead class="table-dark">
+      <tr>
+        <th>User ID</th>
+        <th>Name</th>
+        <th>Address</th>
+		    <th>Contact</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      $sqlListofUsers="select * from users";
+      $result=$conn->query($sqlListofUsers);
+          while($userList=$result->fetch_assoc()){ 
+      ?>
+      <tr>
+        <td><?=$userList['User_number']?></td>
+        <td><?=$userList['Name']?></td>
+        <td><?=$userList['Address']?></td>
+        <td><?=$userList['contact']?></td>
+      </tr>
+      <?php 
+          }
+      ?>
+    </tbody>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
-	
-    
-
-
+  </table>
+</div>
 
 
 </body>
